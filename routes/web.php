c132a0aa122351312c823402291bd879b1ac2f5f
sayedsafwet77 <sayedsafwet4@gmail.com>
2022-06-29 12:28:14 +0200

    revit test Change

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,6 @@ Route::prefix('/dashboard')->name('dashboard.')->namespace('DASHBOARD')->group(f
     Route::get('/device/delete/all','CarDeviceController@deleteAll')->name('device.delete.all');
     Route::post('/device/search','CarDeviceController@search')->name('device.search');
 
-    Route::post('/device/search','CarDeviceController@search')->name('device.search');
 
 
 });
