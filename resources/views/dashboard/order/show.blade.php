6d723b897413b217443dc78afefb44fb415446f1
bassantsalam <bassantmostafa225@gmail.com>
2022-07-03 13:53:29 +0200

    order start front- controller

---
+++ resources/views/dashboard/order/show.blade.php
@@ -0,0 +1,53 @@
+@extends('dashboard.layouts.main')
+@push('title')   طلب   {{ $row->Client->fn_name  }} تأجير سيارة -  {{$row->Car->code }} @endpush
+@push('header')     طلب   {{ $row->Client->fn_name  }} تأجير سيارة -  {{$row->Car->code }}@endpush
+@section('content')
+
+<div class="row gutters">
+    <div class="col-md-12 text-right mb-2">
+        <a class="btn btn-info btn-rounded p-1 pr-2 pl-2" href="{{ URL::route('dashboard.user.index') }}"><span class="w-100 icon-arrow-left text-light" style="font-size: 1.3rem"></span></a>
+    </div>
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <div class="card">
+            <div class="row">
+            </div>
+            <div class="card-header bg-info">
+                <div class="card-title text-center text-light pb-1">عرض بيانات المستخدم {{ $row->name }}</div>
+            </div>
+            <form class="card-body"enctype="multipart/form-data">
+                @csrf
+                @method('POST')
+                @include('dashboard.order.form')
+
+
+                    <div class="row gutters">
+                        <div class="col-xl-12 col-lglg-12 col-md-12 col-sm-12 col-12">
+                            <div class="form-group">
+                                <label for="image">   </label>
+                                <img id="image" style="height:200px" class="img-fluid rounded" alt="Responsive image" src="{{ URL::asset($row->image) }}">
+                            </div>
+                        </div>
+
+                    </div>
+
+            </form>
+        </div>
+    </div>
+
+</div>
+@endsection
+@push('js')
+ <script>
+    $(document).ready(function(){
+        $('#message').delay(3000).fadeOut('slow');
+        $('#name').val("{{ $row->name }}").prop('disabled', true);
+        $('#type').val("{{ $row->type }}").prop('disabled', true);
+        $('#phone').val("{{ $row->phone }}").prop('disabled', true);
+        $('#email').val("{{ $row->email }}").prop('disabled', true);
+    });
+
+ </script>
+
+
+
+@endpush
