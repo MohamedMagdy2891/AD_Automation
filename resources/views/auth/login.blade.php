c251d3d08b04b472d14429d82cd5c49cce05ec20
sayedsafwet77 <sayedsafwet4@gmail.com>
2022-07-06 15:47:10 +0200

    login route and view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,57 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Meta -->
+		<meta name="description" content="Responsive Bootstrap4 Dashboard Template">
+		<meta name="author" content="ParkerThemes">
+		<link rel="shortcut icon" href="{{ URL::asset('assets/img/icon.png')}}" />
+
+		<title>4GO Dashboard - تسجيل الدخول</title>
+
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css')}}" />
+
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/main.css')}}" />
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+        <style>
+            body{
+                background-color: rgba(39, 39, 39, 0.8);
+                background-blend-mode: soft-light;
+                background-image: url('{{ URL::asset("assets/img/automation.jpg")}}');
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                background-size: cover;
+
+            }
+            *{
+                font-family: 'Cairo', sans-serif !important;
+            }
+        </style>
+
+	</head>
+
+	<body class="authentication">
+
+		<!-- Container start -->
+		<div class="container">
+
+			<form action="">
+				<div class="row justify-content-md-center ">
+					<div class="col-xl-4 col-lg-5 col-md-6 col-sm-12 ">
+
+					</div>
+				</div>
+
+			</form>
+
+		</div>
+
+	</body>
+
+</html>
